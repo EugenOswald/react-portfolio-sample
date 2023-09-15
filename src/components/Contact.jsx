6a82cef0f4d5b157bc72ed8a3be5669bb5001d08
Eugen Oswald <eugen.oswald@kemweb.de>
2023-09-15 16:24:11 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+import contactImg from '../assets/img/contact-img.svg';
+import { response } from 'express';
 
 const Contact = () => {
 	const formInitialDetails = {
@@ -10,8 +13,103 @@ const Contact = () => {
 	};
 
 	const [formDetails, setFormDetails] = useState(formInitialDetails);
+	const [butttonText, setButtonText] = useState('Send');
+	const [status, setStatus] = useState({});
 
-	return <div>Contact</div>;
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setButtonText('Sending...');
+		let reponse = await fetch('http://localhost:5000/contact', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'Application/json;charset=utf-8',
+			},
+			body: JSON.stringify(formDetails),
+		});
+		setButtonText('Send');
+		let result = response.json();
+		setFormDetails(formInitialDetails);
+		if (result.code === 200) {
+			setStatus({ success: true, message: 'Message sent successfully' });
+		} else {
+			setStatus({ success: false, message: ' Somethin went wrong, please try again later.' });
+		}
+	};
+
+	const onFormUpdate = (category, value) => {
+		setFormDetails({
+			...formDetails,
+			[category]: value,
+		});
+	};
+
+	return (
+		<section className='contact' id='connect'>
+			<Container>
+				<Row className='align-items-center'>
+					<Col md={6}>
+						<img src={contactImg} alt='Contact Us' />
+					</Col>
+					<Col md={6}>
+						<h2>Get in Touch</h2>
+						<form onSubmit={handleSubmit}>
+							<Row>
+								<Col sm={6} className='px-1'>
+									<input
+										type='text'
+										value={formDetails.firstName}
+										placeholder='First Name'
+										onChange={(e) => onFormUpdate('firstName', e.target.value)}
+									/>
+								</Col>
+								<Col sm={6} className='px-1'>
+									<input
+										type='text'
+										value={formDetails.lastName}
+										placeholder='Last Name'
+										onChange={(e) => onFormUpdate('lastName', e.target.value)}
+									/>
+								</Col>
+								<Col sm={6} className='px-1'>
+									<input
+										type='email'
+										value={formDetails.email}
+										placeholder='eMail'
+										onChange={(e) => onFormUpdate('email', e.target.value)}
+									></input>
+								</Col>
+								<Col sm={6} className='px-1'>
+									<input
+										type='tel'
+										value={formDetails.phone}
+										placeholder='Phone'
+										onChange={(e) => onFormUpdate('phone', e.target.value)}
+									></input>
+								</Col>
+								<Col sm={6} className='px-1'>
+									<textarea
+										rows={6}
+										type='text'
+										value={formDetails.message}
+										placeholder='Message'
+										onChange={(e) => onFormUpdate('message', e.target.value)}
+									></textarea>
+									<button type='submit'>
+										<span>{butttonText}</span>
+									</button>
+								</Col>
+								{status.message && (
+									<Col>
+										<p className={status.success === false ? 'danger' : 'success'}>{status.message}</p>
+									</Col>
+								)}
+							</Row>
+						</form>
+					</Col>
+				</Row>
+			</Container>
+		</section>
+	);
 };
 
 export default Contact;
---
+++ src/components/MailchipmForm.jsx
@@ -0,0 +1,12 @@
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+
+
+import React from 'react'
+
+const MailchipmForm = () => {
+  return (
+    <div>MailchipmForm</div>
+  )
+}
+
+export default MailchipmForm
