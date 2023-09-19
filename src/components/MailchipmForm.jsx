3617db9c2cdf80910938028ec8fa40ce41bff385
Eugen Oswald <eugen.oswald@kemweb.de>
2023-09-19 15:34:39 +0200

    add mailchimp+newsletter jsx

--- src/components/MailchipmForm.jsx
+++ src/components/MailchipmForm.jsx
@@ -1,12 +1,22 @@
 import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import Newsletter, { Newslatter } from './Newsletter';
 
-
-import React from 'react'
+import React from 'react';
 
 const MailchipmForm = () => {
-  return (
-    <div>MailchipmForm</div>
-  )
-}
+	constpostUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+	return (
+		<>
+			<MailchimpSubscribe
+				url={postUrl}
+				render={({ subscribe, status, message }) => (
+					<>
+						<Newsletter status={status} message={message} onValidated={(formData = subscribe(fromDate))}></Newsletter>
+					</>
+				)}
+			></MailchimpSubscribe>
+		</>
+	);
+};
 
-export default MailchipmForm
+export default MailchipmForm;
